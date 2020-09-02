0dd85d8e5c2887b5ec275550677a1b4e52d8643c
Blekercm <35551174+Blekercm@users.noreply.github.com>
2020-09-02 17:13:32 -0500

    Version update 4

--- src/webstoreClient.js
+++ src/webstoreClient.js
@@ -3,7 +3,7 @@ import RestClient from './restClient';
 
 export default class WebStoreClient extends RestClient {
 	constructor(options) {
-		super({ baseUrl: 'https://api.blencm.com/v1', token: options.token });
+		super({ baseUrl: 'https://api.cezerin.com/v1', token: options.token });
 	}
 
 	static authorize = (email, adminUrl) => {
@@ -13,7 +13,7 @@ export default class WebStoreClient extends RestClient {
 			headers: { 'Content-Type': 'application/json' },
 		};
 
-		return fetch('https://api.blencm.com/v1/account/authorize', config).then(
+		return fetch('https://api.cezerin.com/v1/account/authorize', config).then(
 			RestClient.returnStatusAndJson
 		);
 	};
